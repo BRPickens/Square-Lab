1b28520e717a3146db1521155a5d4612c914f52a
BRPickens <brpickens@gmail.com>
2017-09-29 11:33:18 -0500

    adding square

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var btn = document.createElement('button');
+    btn.innerText = 'Add Square';
+    btn.appendChild(t);
+    document.body.appendChild(btn);
+
+})
